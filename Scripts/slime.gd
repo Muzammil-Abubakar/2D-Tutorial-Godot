68a67d4a01d6dca92ef55a3e9deab9db962fda9b
Muzammil-Abubakar <muzammil.ab314@gmail.com>
2025-05-23 14:57:22 +0500

    Lesson 7: Enemy Implemented

---
+++ Scripts/slime.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+const SPEED = 60
+@onready var right = $Right
+@onready var left = $Left
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+var direction = 1
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if right.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = true
+		print("right")
+	if left.is_colliding():
+		direction = 1
+		animated_sprite_2d.flip_h = false
+		print("right")
+	position.x += direction * SPEED * delta
