7bcbb736a36da5589f063ee6f2ce75b82ba0a067
Muzammil-Abubakar <muzammil.ab314@gmail.com>
2025-05-26 14:32:39 +0500

    Lesson 9.5: Cotoye Timer and Jump Buffer Implemented

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,30 +1,50 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+const COYOTE_TIME = 0.15  # seconds
+const JUMP_BUFFER_TIME = 0.07  # seconds
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
+# Timers
+var coyote_timer = 0.0
+var jump_buffer_timer = 0.0
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Decrease timers
+	if coyote_timer > 0:
+		coyote_timer -= delta
+	if jump_buffer_timer > 0:
+		jump_buffer_timer -= delta
+
+	# Coyote time logic
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME
+
+	# Jump buffering
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer_timer = JUMP_BUFFER_TIME
+
+	# Apply gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Execute jump if jump was pressed recently AND player is either on floor or within coyote time
+	if jump_buffer_timer > 0 and coyote_timer > 0:
 		velocity.y = JUMP_VELOCITY
+		jump_buffer_timer = 0  # Reset after jump
+		coyote_timer = 0
 
+	# Horizontal movement
 	var direction = Input.get_axis("move_left", "move_right")
-	
+
 	if direction > 0:
 		animated_sprite_2d.flip_h = false
 	elif direction < 0:
 		animated_sprite_2d.flip_h = true
-	
+
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite_2d.play("idle")
@@ -33,7 +53,6 @@ func _physics_process(delta):
 	else:
 		animated_sprite_2d.play("jump")
 
-	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
