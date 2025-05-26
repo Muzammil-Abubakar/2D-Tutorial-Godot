60a17feedfc7950b0c898aadb9ab42a165279e0f
Muzammil-Abubakar <muzammil.ab314@gmail.com>
2025-05-26 15:13:55 +0500

    Lesson 11: Global Sound, Pickup And Sound Buses Implemented

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var game_manager = %"Game Manager"
+@onready var animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,4 +16,4 @@ func _process(delta):
 
 func _on_body_entered(body):
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup")
